fce5a279e41795783f2fedde33dfa363ade503a4
Virandry <virandry@gmail.com>
2017-10-29 11:49:56 +0800

    Add files via upload

---
+++ src/main/java/io/virandry/inficase/InficaseApplication.java
@@ -0,0 +1,12 @@
+package io.virandry.inficase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InficaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InficaseApplication.class, args);
+	}
+}
---
+++ src/test/java/io/virandry/inficase/InficaseApplicationTests.java
@@ -0,0 +1,16 @@
+package io.virandry.inficase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InficaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
